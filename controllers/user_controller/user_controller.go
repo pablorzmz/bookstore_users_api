f52e68d86fe4617357e6d1b0be35d6b8a77e361a
Jose Pablo Ramirez Mendez <jose.ramirez.mendez1@kyndryl.com>
2021-08-16 11:13:25 -0600

    movin controllers

---
+++ controllers/user_controller/user_controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Not ready")
+}
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Not ready")
+}
+
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Not ready")
+}
